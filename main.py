7c8f923ac2ad55ffa26cb3db2eb2b1d23adfb3ee
ppsmoraes <ppsmoraes@gmail.com>
2025-07-28 22:57:48 -0300

    First analys direct on py file

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # type: ignore[import-not-found]
+from pandas import DataFrame, read_excel, to_datetime
 
-df: pd.DataFrame = pd.read_excel('data/01 Call-Center-Dataset.xlsx')
+df: DataFrame = read_excel('data/01 Call-Center-Dataset.xlsx')
 
-df_dayly: pd.DataFrame = df[['Call Id', 'Date']].groupby('Date').count()
+dayly: DataFrame = df[['Call Id', 'Date']].groupby('Date').count()
+dayly.rename(columns={'Call Id': 'Calls'}, inplace=True)
+dayly.index = to_datetime(dayly.index)
+dayly['Day Name'] = dayly.index.day_name()
+dayly['Week'] = dayly.index.isocalendar().week
 
-print(df_dayly)
+day_names: list[str] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+]
 
-# plt.figure(figsize=(12, 6))
-# plt.plot(df_dayly.index, df_dayly['Call Id'])
+plt.figure(figsize=(10, 6))
+plt.boxplot(
+    [dayly[dayly['Day Name'] == day]['Calls'] for day in day_names],
+    tick_labels=day_names,
+)
+plt.xlabel('Dias da Semana')
+plt.ylabel('Ligações Recebidas')
+plt.title('Distribuição de Ligações por Dia da Semana')
+plt.xticks(rotation=45)
+plt.show()
+
+# plt.plot(dayly.index, dayly['Calls'])
 # plt.xlabel('Data')
 # plt.ylabel('Quantidade de Chamadas')
 # plt.title('Chamadas por Dia')
-# plt.xticks(rotation=45)
+# plt.xticks(dayly.index[::8])
 # plt.show()
