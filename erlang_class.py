2461ed3bc67eb057ba2b47430cfa8f028ffa81ae
ppsmoraes <ppsmoraes@gmail.com>
2025-08-01 19:22:04 -0300

    Added methods service_level() and raw_agents_required()

--- erlang_class.py
+++ erlang_class.py
@@ -5,40 +5,23 @@ from math import ceil, exp, factorial
 
 from pandas import DataFrame
 
-# from typing import overload
-
 
 class CallCenterPredictions:
-    """Classe that contains the predictable variables of a call center in a certain period of time.
-
-    In this class you gonna find those propreties:
-        Those ones should be given to the constructor of the class:
-        - id: An index of the information.
-        - start_time: The starting date and time.
-        - end_time: The ending date and time.
-        - calls: The number of expected calls in thar period of time.
-        - aht: The average handling time in seconds.
-
-        From now on, those propeties are calculated based on the premisses.
-        - period: The total time, in seconds, between the `start_time` and `end_time`.
-        - cph: The number of calls per hour.
-        - erlgangs: Traffic intensity in Erlangs.
-    """
+    """Classe that contains the predictable variables of a call center in a certain period of time."""
 
     def __init__(
         self,
-        id: str,
         start_time: datetime,
         end_time: datetime,
         calls: int,
         average_handling_time: int,
+        target_service_level: float,
+        target_answer_time: int,
     ) -> object:
         """Predictable variables of a call center in a certain period of time.
 
         Parameters
         ----------
-        id : str
-            The index.
         start_time : datetime
             The stating date and time.
         end_time : datetime
@@ -47,23 +30,37 @@ class CallCenterPredictions:
             The number of expected calls in thar period of time.
         average_handling_time : int
             The average handling time in seconds.
+        target_service_level : float
+            The service level required in that period of time. This should be a number between 0 and 1 (both ends included).
+        target_answer_time : int
+            The target service level time to answer a call in seconds.
 
         Returns
         -------
         CallCenterPredictions
             The predictable variables of a call center in a certain period of time.
+
+        Raises
+        ------
+        ValueError
+            If ``target_service_level`` given is not a number between 0 and 1 (both ends included).
         """
-        self.id: str = id
         self.start_time: datetime = start_time
         self.end_time: datetime = end_time
         self.calls: int = calls
         self.aht: int = average_handling_time
+        self.tsl: float = target_service_level
+        self.tat: int = target_answer_time
+
+        if self.tsl > 1 or self.tsl < 0:
+            raise ValueError(
+                f'The target service level should be a number between 0 and 1 (both ends included). But {self.tsl} were given.'
+            )
 
         self.period: float = self.period_in_seconds()
         self.cph: float = self.calls_per_hour()
         self.erlangs: float = self.traffic_intensity()
-
-        # self.agents: int =
+        self.raw_agents: int = self.raw_agents_required()
 
     def period_in_seconds(self) -> float:
         """Calculate the period of time in seconds.
@@ -78,10 +75,12 @@ class CallCenterPredictions:
         >>> from erlang_class import CallCenterPredictions
         >>> from datetime import datetime
         >>> predictions = CallCenterPredictions(
-        ...     id=1,
         ...     start_time=datetime(2021, 4, 1, 8),
         ...     end_time=datetime(2021, 4, 1, 8, 30),
         ...     calls=100,
+        ...     average_handling_time=180,
+        ...     target_service_level=0.8,
+        ...     target_answer_time=20
         ... )
         >>> predictions.period_in_seconds()
         1800.0
@@ -110,27 +109,57 @@ class CallCenterPredictions:
         """
         return self.cph * (self.aht / 3600)
 
-    def erlang_c(self) -> float:
+    def erlang_c(self, agents: int) -> float:
         # TODO Add exemples
-        """
-        Calculate the probability that a call will wait in queue (Erlang C formula).
+        """Calculate the probability that a call will wait in queue (Erlang C formula).
+
+        Parameters
+        ----------
+        agents : int
+            The quantity of agents to answer those calls.
 
         Returns
         -------
         float
             Probability that a call will wait in queue (Erlang C).
         """
-        _num: float = self.erlangs**self.agents / factorial(self.agents)
-        _den: float = (1 - self.erlangs / self.agents) * sum(
-            self.erlangs**i / factorial(i) for i in range(self.agents)
+        _num: float = self.erlangs**agents / factorial(agents)
+        _den: float = sum(self.erlangs**_ / factorial(_) for _ in range(agents)) * (
+            1 - self.erlangs / agents
         )
         return _num / (_den + _num)
 
+    def service_level(self, agents: int) -> float:
+        # TODO Add exemples
+        """Calculate the estimated service level.
+
+        Parameters
+        ----------
+        agents : int
+            The quantity of agents to answer those calls.
+
+        Returns
+        -------
+        float
+            Estimated service level (probability that a call is answered within the target time).
+        """
+        return 1 - (
+            self.erlang_c(agents) * exp((self.erlangs - agents) * (self.tat / self.aht))
+        )
+
+    def raw_agents_required(self) -> int:
+        # TODO Create docstring
+        agents: int = ceil(self.erlangs)  # Initial guess for agents
+        while self.service_level(agents) < self.tsl:
+            agents += 1
+        return agents
+
     def to_pandas(self) -> DataFrame:
         # TODO Add docstring
-        return DataFrame([self.__dict__]).set_index('id')
+        return DataFrame([self.__dict__])
 
     def __str__(self) -> str:
+        # TODO Add docstring
         return str(self.to_pandas())
 
 
@@ -138,11 +167,12 @@ class CallCenterPredictions:
 # For developer phase only
 def main() -> None:
     predictions = CallCenterPredictions(
-        id=1,
         start_time=datetime(2021, 4, 1, 8),
         end_time=datetime(2021, 4, 1, 8, 30),
         calls=100,
         average_handling_time=180,
+        target_service_level=0.8,
+        target_answer_time=20,
     )
     print(predictions)
 
